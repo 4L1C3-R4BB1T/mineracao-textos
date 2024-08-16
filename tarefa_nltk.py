9940e98ed5a60a3d4632354cab1ebe992a5d1df0
Livia <livinha.guimaraes.013@gmail.com>
2024-08-16 20:28:05 -0300

    :sparkles: update

--- tarefa_nltk.py
+++ tarefa_nltk.py
@@ -1,11 +1,9 @@
-# Pré-processamento com NLTK
-
-```py
 import re
 import string
 import nltk
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
+from nltk.tokenize import RegexpTokenizer
 from nltk.stem import PorterStemmer
 from nltk.stem import WordNetLemmatizer
 
@@ -22,21 +20,17 @@ with open('no14011801.txt', 'r', encoding='utf-8') as texto:
 
 # retorna o array com as palavras do texto
 palavras = word_tokenize(tribuna)
-```
 
-## Remover stopwords
+"""# **Remover stopwords**"""
 
-```py
 # compara cada palavra do texto pra ver se e uma stopword
 texto_sem_stopwords = [palavra for palavra in palavras if not palavra.lower() in stopwords]
 
 # juntas todas as palavras do array
 ' '.join(texto_sem_stopwords)
-```
 
-## Remover sinais de pontuação
+"""# **Remover sinais de pontuação**"""
 
-```py
 # compara cada palavra do texto pra ver se e um sinal de pontuacao
 texto_sem_pontuacao = [palavra for palavra in palavras if not palavra in string.punctuation]
 
@@ -45,37 +39,26 @@ texto_sem_pontuacao = [palavra for palavra in palavras if not palavra in string.
 # texto_sem_pontuacao = tokenizer.tokenize(tribuna)
 
 ' '.join(texto_sem_pontuacao)
-```
 
-## Remover caracteres especiais (exceto emoji)
+"""# **Remover caracteres especiais (exceto emoji)**"""
 
-```py
 # compara cada palavra do texto pra ver se e um caractere especial
 texto_sem_caracteres_especiais = [re.sub(r'[^\w\s\U0001F600-\U0001F64F\U0001F300-\U0001F5FF\U0001F680-\U0001F6FF\U0001F700-\U0001F77F]', '', palavra) for palavra in palavras]
 
 ' '.join(texto_sem_caracteres_especiais)
-```
 
-## Aplicar stemização
+"""# **Aplicar stemização**"""
 
-Reduzir a palavra à sua raiz ou radical.
-
-```py
 stemmer = PorterStemmer()
 
 texto_com_stemizacao = [stemmer.stem(palavra) for palavra in palavras]
 
 ' '.join(texto_com_stemizacao)
-```
 
-## Aplicar lematização
+"""# **Aplicar lematização**"""
 
-Palavras em sua forma infinitiva dos verbos e no masculino singular dos substantivos e adjetivos.
-
-```py
 lemmatizer = WordNetLemmatizer()
 
 texto_com_lematizacao = [lemmatizer.lemmatize(palavra) for palavra in palavras]
 
 ' '.join(texto_com_lematizacao)
-```
